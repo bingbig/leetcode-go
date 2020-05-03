f2611c90f99ea534886f08660c8d373c1fdb569e
bingbig <598604711@qq.com>
2020-05-03 19:07:16 +0800

    1

--- 剑指offer/38/solution.go
+++ 剑指offer/38/solution.go
@@ -5,5 +5,24 @@ func permutation(s string) []string {
 		return []string{s}
 	}
 
-	return nil
+	str := []rune(s)
+	var res, ps []string
+	for i := 0; i < len(s); i++ {
+		str[0], str[i] = str[i], str[0]
+		str0 := string(str[0])
+		rs := permutation(string(str[1:]))
+		for _, r := range rs {
+			res = append(res, str0+r)
+		}
+	}
+
+	tmp := map[string]bool{}
+	for _, s := range res {
+		if _, ok := tmp[s]; !ok {
+			tmp[s] = true
+			ps = append(ps, s)
+		}
+	}
+
+	return ps
 }
--- 剑指offer/38/solution_test.go
+++ 剑指offer/38/solution_test.go
@@ -1,1 +1,11 @@
 package solution
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPermutation(t *testing.T) {
+	fmt.Printf("%v\n", permutation("abc"))
+	fmt.Printf("%v\n", permutation("aab"))
+}
